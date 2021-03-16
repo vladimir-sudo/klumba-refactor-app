729a1871358c74e34f3fb2051559c6371ce23400
Vladimir Malets <vladimirmalec50@gmail.com>
2021-03-17 01:10:47 +0200

    do refactor StringFormatter

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -12,36 +12,11 @@ use Kl\Traits\StringFormatter;
  */
 class Model
 {
-    use StringFormatter;
-
-    const FIELDS_TYPE_CAMEL_CASE = 'camelCase';
-
-    const FIELDS_TYPE_LOWER_CASE = 'lowerCase';
-
     /**
-     * @param string $fieldsFormat
      * @return array
      */
-    public function toArray($fieldsFormat = null): array
+    public function toArray(): array
     {
-        $result = [];
-
-        foreach ((array)$this as $key => $value)
-        {
-            $keyFormatted = $this->format($key);
-
-            switch ($fieldsFormat) {
-                case $fieldsFormat === self::FIELDS_TYPE_CAMEL_CASE:
-                    $keyFormatted = lcfirst($keyFormatted);
-                    break;
-                case $fieldsFormat === self::FIELDS_TYPE_LOWER_CASE:
-                    $keyFormatted = strtolower($keyFormatted);
-                    break;
-                default:
-                    $keyFormatted = $key;
-            }
-            $result[$keyFormatted] = $value;
-        }
-        return $result;
+        return (array)$this;
     }
 }
